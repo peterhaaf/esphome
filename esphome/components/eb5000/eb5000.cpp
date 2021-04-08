9cbf8364258ebd22279c46feceba6f5859e2cd99
Peter Haaf <peter.haaf@sap.com>
2021-04-08 08:28:29 +0200

    ii

--- esphome/components/eb5000/eb5000.cpp
+++ esphome/components/eb5000/eb5000.cpp
@@ -14,13 +14,13 @@ void EB5000::loop() {
   //  this->last_read_ = now;
   //}
   uint8_t buffer[128];
-  while (1) {
+  
     while (this->available()<10) {}
     this->read_array(buffer,this->available());
     //ESP_LOGD(TAG, "read: %c %i", this->read(), this->available());
     buffer[this->available()] = 0;
     ESP_LOGD(TAG, "read: %s", buffer); 
-  }
+  
   // PZEM004T packet size is 7 byte
   while (this->available() >= 7) {
     auto resp = *this->read_array<7>();
