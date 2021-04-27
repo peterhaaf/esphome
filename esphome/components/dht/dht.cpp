b7f77c73d363ab85d0db3a81290df84c660fed5e
peterhaaf <34441417+peterhaaf@users.noreply.github.com>
2021-04-27 15:05:23 +0200

    Update dht.cpp

--- esphome/components/dht/dht.cpp
+++ esphome/components/dht/dht.cpp
@@ -95,8 +95,8 @@ bool HOT ICACHE_RAM_ATTR DHT::read_sensor_(float *temperature, float *humidity, 
     } else if (this->model_ == DHT_MODEL_DHT22_TYPE2) {
       delayMicroseconds(2000);
     } else {
-      delayMicroseconds(3000);
-      ESP_LOGW(TAG, "AM2302-3000");
+      delayMicroseconds(1);
+      ESP_LOGW(TAG, "AM2302-1");
     }
     this->pin_->pin_mode(INPUT_PULLUP);
     delayMicroseconds(40);
